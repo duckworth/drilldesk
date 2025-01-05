8c06ed623a6f93325b5a32baecf7a01e5b885993
John Lemp <john@graavy.com>
2025-01-05 12:44:16 -0500

    application layout WIP

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    flash[:notice] = "Welcome to the home page!"
   end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,5 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "flowbite/dist/flowbite.turbo.js";
+import "./dark_mode"
+
